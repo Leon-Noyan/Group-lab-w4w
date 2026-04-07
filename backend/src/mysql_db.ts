942a46816c5d621ac9725c4683add26c526f5df8
Leon <108742319+Leon-Noyan@users.noreply.github.com>
2026-04-07 13:08:06 +0200

    Use environment variable for database name

--- backend/src/mysql_db.ts
+++ backend/src/mysql_db.ts
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     port: Number(process.env.DB_PORT) || 3306, 
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS!,
-    database: 'music_app',
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
